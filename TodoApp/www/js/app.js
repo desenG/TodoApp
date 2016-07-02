61c58bd2ab5f20775bd5f729c48fd8feba5b9c5b
jason <jason.desen.guo@gmail.com>
2016-07-02 13:31:24 -0400

    some fixed

--- TodoApp/www/js/app.js
+++ TodoApp/www/js/app.js
@@ -8,6 +8,10 @@ angular.module('starter', ['ionic', 'starter.controllers','ngCordova','LocalStor
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    if(device.platform === "iOS") {
+        window.plugin.notification.local.promptForPermission();
+    }
+                       
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
